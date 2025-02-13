5e65a463488497b04e2314b6fa293f94d4f71011
Janelle Chloe Francisco <janelle.francisco@student.ateneo.edu>
2025-02-13 21:26:27 +0800

    Correct variable names and fix typos

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipe-list'),
-    path('recipe/1', views.recipe_one, name='recipe-one'),
-    path('recipe/2', views.recipe_two, name='recipe-two'),
+    path('recipe/1/', views.recipe_one, name='recipe-one'),
+    path('recipe/2/', views.recipe_two, name='recipe-two'),
 ]
 
 app_name = 'ledger'
