efb0843f29e4eed1ed9336d781bfecb10b038c41
Janelle Chloe Francisco <janelle.francisco@student.ateneo.edu>
2025-02-13 20:43:14 +0800

    Create template for recipe list

---
+++ recipebook/ledger/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.recipe_list, name='recipe-list'),
+    path('/recipe/1', views.recipe_one, name='recipe-one'),
+    path('/recipe/2', views.recipe_two, name='recipe-two'),
+]
