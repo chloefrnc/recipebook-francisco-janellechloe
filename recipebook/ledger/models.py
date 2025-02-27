6d72f33e609181bc1d0522f050de1b1e4016db98
Janelle Chloe Francisco <janelle.francisco@student.ateneo.edu>
2025-02-27 20:25:06 +0800

    Add fields to models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,15 +1,41 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    class Meta:
+        verbose_name = 'ingredient'
+        verbose_name_plural = 'ingredients'
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return'{}'.format(self.name)
+
+    def get_absolute_url(self):
+        reverse('recipe', args=[str(self.name)])
+    
+    class Meta:
+        verbose_name = 'recipe'
+        verbose_name_plural = 'recipes'
+
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipe'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .views import recipe_list
 
 urlpatterns = [
-    path('', views.recipe_list, name='recipe-list'),
+    path('<int:id>', recipe_list, name='recipe-list'),
     path('recipe/1/', views.recipe_one, name='recipe-one'),
     path('recipe/2/', views.recipe_two, name='recipe-two'),
 ]
