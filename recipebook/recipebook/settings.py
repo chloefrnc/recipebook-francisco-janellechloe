14aee6d639855b4bf4719bd7dc48371b89f49edb
Janelle Chloe Francisco <janelle.francisco@student.ateneo.edu>
2025-03-10 20:30:09 +0800

    Remove password reset flow, update settings.py to include redirect urls

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+LOGIN_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = ''
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
