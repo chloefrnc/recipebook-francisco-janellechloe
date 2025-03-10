3fe1993bb470bfbcc521518f12afd3814c7c8c3e
Janelle Chloe Francisco <janelle.francisco@student.ateneo.edu>
2025-03-10 19:12:58 +0800

    Update timezone in settings, update app-level urls to utilize class-based views

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import recipe_list, recipe_detail
+from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipes/', recipe_list, name="recipe_list"),
-    path('recipe/<int:pk>', recipe_detail, name='recipe_detail')
+    path('recipes/', RecipeListView.as_view(), name="recipe_list"),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail')
 ]
 
 app_name = 'ledger'
