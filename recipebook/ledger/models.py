e04dcebd68b6b44c554bb968b305e099337027c7
Janelle Chloe Francisco <janelle.francisco@student.ateneo.edu>
2025-03-24 14:45:12 +0800

    Modify settings to include media, create RecipeImage model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -53,3 +53,13 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f'{self.ingredient.name}, {self.quantity}'
+
+
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to='images/', null=False)
+    description = models.TextField(max_length=255)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipe'
+    )
--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -121,10 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 LOGIN_REDIRECT_URL = '/recipes/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
